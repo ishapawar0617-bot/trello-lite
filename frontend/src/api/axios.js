2625c56e060bd1d2adf4ae22ad0f04f88840e26d
Isha Pawar <ishapawar0617@gmail.com>
2026-06-01 23:02:30 +0530

    Update API URL for EC2

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: "http://3.111.33.32:5000/api",
 });
 
 API.interceptors.request.use((req) => {
